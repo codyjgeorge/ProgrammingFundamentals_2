df9e03cb0ffdd8274ee7f58f515ab2e6af45727e
codyjgeorge <codygeorge315@gmail.com>
2025-01-14 21:09:26 -0600

    week 1 program complete

---
+++ week1program.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// creates main function
+int main() {
+  int num; // creates num variable
+
+  cout << "codyGeorge 14Jan25" << endl; // prints out string and ends line
+
+  cout << "What's your favorite number: "; // asks user's input for favorite
+                                           // number
+  cin >> num; // stores users fav number in variable num
+
+  cout << "Your favorite number is: " << num
+       << endl; // prints string and variable num's value
+
+  return 0;
+}
