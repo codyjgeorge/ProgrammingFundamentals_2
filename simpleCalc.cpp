2a17f71da846263b1687cd8fe2e1f762148a7559
codyjgeorge <codygeorge315@gmail.com>
2025-01-15 19:06:25 -0600

    started week 2 simple calulator

---
+++ simpleCalc.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int num1;
+  int num2;
+
+  cout << "Welcome to the Simple Calculator!" << endl;
+
+  cout << "Please enter the first number: ";
+  cin >> num1;
+
+  cout << "Please enter the second number: ";
+  cin >> num2;
+
+  cout << "Calculating..." << endl;
+}
