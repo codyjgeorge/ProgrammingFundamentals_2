e7ff9cd562c49c436ce57f5ad0fd6dc1bc921c26
CodyG <codygeorge315@gmail.com>
2025-02-13 14:01:47 -0600

    hexConverter edited from class

--- hexConverter.cpp
+++ hexConverter.cpp
@@ -1,31 +1,75 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
-int decimal(hex) {
-  char hex;
-  hex = toupper(hex);
+const int MAX_LENGTH = 8;
+bool hasError = false;
 
-  if (hex <= 'F' && hex >= 'A') {
-    int value = 10 + hex - 'A';
-  } else if (isdigit(hex)) {
-  }
-
-  return hex;
-};
+bool validateHexInput(string &hexNum);
+int hextoDecimal(char hexDigit);
+int hextoDecimal(char hexDigit, bool);
+inline int updateDecimal(int &decimal, const int &digitValue);
+inline void displayResults(string &hexNumber, int &decimalResults);
 
 int main() {
-  char hexa;
-  cout << "Welcome to the Hexadecimal to Decimal Converter!\n" << endl;
-  cout << "Please enter a hexa decimal number: ";
-  cin >> hexa;
+  string hexNumber;
+  
+  cout << "Enter a hexadecimal number (max " << MAX_LENGTH << " digits): ";
+  cin >> hexNumber;
+ if (validateHexInput(hexNumber)) { 
+  int decimal = 0;
+  for (char c : hexNumber) {
+      int digitValue;
+      if (islower(c)) {
+         digitValue = hextoDecimal(c, true);     
+      } else {
+	 digitValue = hextoDecimal(c);
+      }
+      decimal = updateDecimal(decimal, digitValue);
+  }
 
-  cout << "\nConverting " << hexa << " to a decimal..." << endl;
-
-  cout << "\nThe decimal equivalent of hexadecimal " << hexa << " is "
-       << decimal(hexa) << endl;
-
-  cout << "\nThank you for using the Converter!";
+  displayResults(hexNumber, decimal);
+ } else {
+	 cout << "Invalid hexadecimal number! Please try again." << endl;
+ }
 
   return 0;
+}
+
+bool validateHexInput(string &hexNum) {
+    if (hexNum.length()== 0 || hexNum.length() > MAX_LENGTH) {
+	    hasError = true;
+	    return false;
+    }
+
+    for (char c : hexNum) {
+	    if(!((c >= '0' && c <= '9') ||
+	         (c >= 'A' && c <= 'F') ||
+		 (c >= 'a' && c <= 'f'))) {
+		    hasError = true;
+		    return false;
+		}
+} return true;
+}
+
+int hextoDecimal(char &hexDigit) {
+	if (hexDigit >= '0' && hexDigit <= '9') {
+	    return hexDigit - '0';
+	} else {
+	    return hexDigit - 'A' + 10;
+	}
+}
+int hextoDecimal(char &hexDigit, bool) {
+    return hexDigit - 'a' + 10;
+}
+
+inline int updateDecimal(int &decimal, const int &digitValue) {
+    static int multiplier = 16;       
+    return decimal * multiplier + digitValue;
+}
+
+inline void displayResults(string &hexNumber, int &decimalResults) {
+	cout << "Original Hex String Value: " << hexNumber << endl;
+	cout << "Total Decimal Value Results: " << decimalResults << endl;
 }
