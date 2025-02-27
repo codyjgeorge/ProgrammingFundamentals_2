4628d945adfc648b526cecb50b3588fe62086ab7
CodyG <codygeorge315@gmail.com>
2025-02-27 12:47:46 -0600

    sudokuChecker sum of rows

--- sudokuChecker.cpp
+++ sudokuChecker.cpp
@@ -8,7 +8,7 @@ const int columnSize = 3;
 const int rowSum = 6;
 const int columnSum = 6;
 int sudoku[rowSize][columnSize] = {};
-void sum(int sudoku[][columnSize], int rowSize);
+void rowChecker(int sudoku[][columnSize], int rowSize);
 
 int main() {
 
@@ -27,10 +27,10 @@ int main() {
     }
     cout << endl;
   }
-  sum(sudoku, rowSize);
+  rowChecker(sudoku, rowSize);
   return 0;
 }
-void sum(int sudoku[][columnSize], int rowSize) {
+void rowChecker(int sudoku[][columnSize], int rowSize) {
   int sum = 0;
   for (int row = 0; row < rowSize; row++) {
     for (int column = 0; column < columnSize; column++) {
