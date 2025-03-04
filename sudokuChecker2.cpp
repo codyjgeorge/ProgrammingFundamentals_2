82af5e4b98e54da30fee841dc35bb84a6c2f90ab
CodyG <codygeorge315@gmail.com>
2025-03-03 22:01:29 -0600

    gridchecker function wip

--- sudokuChecker2.cpp
+++ sudokuChecker2.cpp
@@ -2,7 +2,6 @@
 
 using namespace std;
 
-bool isValid3x3();
 bool gridChecker(int validSudoku[][9]);
 const int total = 45;
 
@@ -14,31 +13,46 @@ int main() {
       {4, 7, 2, 5, 1, 3, 6, 8, 9}, {6, 1, 8, 9, 7, 2, 4, 3, 5},
       {7, 8, 6, 2, 3, 5, 9, 1, 4}, {1, 5, 4, 7, 9, 6, 8, 2, 3},
       {2, 3, 9, 8, 4, 1, 5, 6, 7}};
-  return 0;
+
+  if (gridChecker(validSudoku) == true) {
+    cout << "Valid Sudoku" << endl;
+  } else {
+    cout << "Invalid Sudoku" << endl;
+  }
 }
 
 bool gridChecker(int validSudoku[][9]) {
+  int counter = 0;
   int startRow = 0;
   int startCol = 0;
   int endRow = 3;
   int endCol = 3;
   int sum = 0;
-  for (int i = 0; i < 9; i++) {
+  while (counter++ < 9) {
+    sum = 0;
     for (int row = startRow; row < endRow; row++) {
       for (int column = startCol; column < endCol; column++) {
         sum += validSudoku[row][column];
       }
     }
-    startRow += 3;
-    endRow += 3;
-    if (endRow == 12) {
-      startRow = 0;
-      endRow = 3;
-    } else if (endCol == 12) {
+    if (sum != 45) {
+      return false;
+    }
+    counter++;
+    startCol += 3;
+    endCol += 3;
+    if (startCol == 9) {
       startCol = 0;
-      endCol = 3;
+      endCol = 0;
+      startRow += 3;
+      endRow += 3;
+    } else if (startRow == 9) {
+      break;
     }
   }
-
-  return true;
+  if (sum == 45) {
+    return true;
+  } else {
+    return false;
+  }
 }
