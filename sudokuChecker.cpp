4c006e19fdc7034a505e67d8165d9a0310a99d05
codyjgeorge <codygeorge315@gmail.com>
2025-03-04 19:28:02 -0600

    sudokuChecker complete

--- sudokuChecker.cpp
+++ sudokuChecker.cpp
@@ -10,7 +10,7 @@ const int total = 45;
 int sudoku[rowSize][columnSize] = {};
 void rowChecker(int sudoku[][columnSize], int rowSize);
 void colChecker(int sudoku[][columnSize], int rowSize);
-void gridChecker(int sudoku[][columnSize], int rowSize);
+bool gridChecker(int sudoku[][columnSize]);
 
 int main() {
 
@@ -31,7 +31,11 @@ int main() {
   }
   rowChecker(sudoku, rowSize);
   colChecker(sudoku, rowSize);
-  gridChecker(sudoku, rowSize);
+  if (gridChecker(sudoku) == true) {
+    cout << "Valid Sudoku" << endl;
+  } else {
+    cout << "Invalid Sudoku" << endl;
+  }
   return 0;
 }
 void rowChecker(int sudoku[][columnSize], int rowSize) {
@@ -43,6 +47,8 @@ void rowChecker(int sudoku[][columnSize], int rowSize) {
 
     if (sum != rowSum) {
       cout << "Failed on row " << row + 1 << endl;
+    } else {
+      cout << "Row " << row + 1 << "'s total is " << sum << endl;
     }
     sum = 0;
   }
@@ -56,15 +62,45 @@ void colChecker(int sudoku[][columnSize], int rowSize) {
 
     if (sum != columnSum) {
       cout << "Failed on column " << column + 1 << endl;
+    } else {
+      cout << "Column " << column + 1 << "'s total is " << sum << endl;
     }
     sum = 0;
   }
 }
-void gridChecker(int sudoku[][columnSize], int rowSize) {
+bool gridChecker(int sudoku[][columnSize]) {
+  int counter = 0;
+  int startRow = 0;
+  int startCol = 0;
+  int endRow = 3;
+  int endCol = 3;
   int sum = 0;
-  for (int row = 0; row < 3; row++) {
-    for (int column = 0; column < 3; column++) {
-      sum += sudoku[row][column];
+  while (counter < 9) {
+    sum = 0;
+    for (int row = startRow; row < endRow; row++) {
+      for (int column = startCol; column < endCol; column++) {
+        sum += sudoku[row][column];
+      }
     }
+    if (sum != 45) {
+      return false;
+    }
+    counter++;
+    cout << "Grid " << counter << "'s total is " << sum << endl;
+    startCol += 3;
+    endCol += 3;
+    if (startCol == 9) {
+      startCol = 0;
+      endCol = 3;
+      startRow += 3;
+      endRow += 3;
+    } else if (startRow == 9) {
+      break;
+    }
+  }
+  if (sum == 45) {
+    return true;
+  } else {
+    return false;
   }
 }
