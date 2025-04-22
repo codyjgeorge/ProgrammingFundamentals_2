f944d2695400969c9cfa70804aa971f27abdf60a
CodyG <codygeorge315@gmail.com>
2025-04-22 01:04:52 -0500

    calculates addition and subtraction of complex numbers

--- week_12/complexNumber.cpp
+++ week_12/complexNumber.cpp
@@ -28,29 +28,22 @@ public:
 
   const double getIm() { return imaginary; }
 
-  // display complex number;
-  void display() { cout << real << imaginary << "i" << endl; }
-
-  // adds first complex number with second complex number;
-  void add(Complex a, Complex b) {
-    double realNum = a.real + b.real;
-    double imNum = a.imaginary + b.imaginary;
-    Complex c = Complex(realNum, imNum);
-    cout << c << endl;
+  // overloaded member functions for calculations;
+  Complex operator+(Complex &b) {
+    return Complex(real + b.real, imaginary + b.imaginary);
+  }
+  Complex operator-(Complex &b) {
+    return Complex(real - b.real, imaginary - b.imaginary);
   }
 
-  // overloaded functions;
-  friend ostream &operator<<(ostream &output, Complex &c) {
-    output << c.real << "+" << c.imaginary << "i" << endl;
+  // overloaded non-member function for output;
+  friend ostream &operator<<(ostream &output, Complex &a) {
+    if (a.imaginary < 0) {
+      output << a.real << a.imaginary << "i" << endl;
+    } else {
+      output << a.real << "+" << a.imaginary << "i" << endl;
+    }
     return output;
-  }
-
-  friend istream &operator>>(istream &input, Complex &a) {
-    double realNum, imNum;
-    input >> realNum >> imNum;
-    a.setReal(realNum);
-    a.setIm(imNum);
-    return input;
   }
 };
 
@@ -61,22 +54,31 @@ int main() {
   double secondImaginary;
   Complex a;
   Complex b;
-  Complex c;
 
-  cout << "Welcome to the simple complex calculator!" << endl << endl;
+  cout << "Welcome to the simple complex number calculator!" << endl << endl;
 
   // asks user to enter first complex number;
   cout << "Enter the first real number and first imaginary number of the first "
           "complex number: "
        << endl;
-  cin >> a;
+  cin >> firstReal >> firstImaginary;
+  a.setReal(firstReal);
+  a.setIm(firstImaginary);
   cout << "You entered: " << a << endl;
 
   // asks user to enter second complex number;
   cout << "Enter the second real number and second imaginary number of the "
           "second complex number: "
        << endl;
-  cin >> b;
+  cin >> secondReal >> secondImaginary;
+  b.setReal(secondReal);
+  b.setIm(secondImaginary);
   cout << "You entered: " << b << endl;
+
+  // calculations +,-,*,/ ;
+  Complex c = a + b;
+  cout << "Total: " << c << endl;
+  c = a - b;
+  cout << "Difference: " << c << endl;
   return 0;
 }
